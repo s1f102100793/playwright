f36f781aa43d89de7e4d2e0d8892b1850e005abe
s1f102100793 <85666759+s1f102100793@users.noreply.github.com>
2023-06-25 00:57:33 +0900

    完成のはず

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import fetchStockPrice from './utils/fetchStockPrice'
 
 async function main() {
-  await fetchStockPrice();
-}
-
-main();
+    while (true) {
+      await fetchStockPrice();
+      await new Promise((resolve) => setTimeout(resolve,1 * 60 * 1000)); // 5 分の待機
+    }
+  }
+  
+  main();
